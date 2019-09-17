68172d2ff2fcfae48a4f4bcdc3e83402085290e0
Leandro Morocho <leandro.morocho@tecsup.edu.pe>
2019-09-17 14:10:36 -0500

    Initial Commit

---
+++ canvas2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  canvas2
+//
+//  Created by mbtec22 on 9/17/19.
+//  Copyright © 2019 mbtec22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
